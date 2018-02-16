15bdeb3e6e1bcf520b8982bcabb6866b02eb0e0e
Valeryi Savich <Relrin78@gmail.com>
2018-02-16 20:42:35 +0300

    Update mongodb.py

--- sanic-app/app/extensions/mongodb.py
+++ sanic-app/app/extensions/mongodb.py
@@ -14,6 +14,12 @@ class MongoDbExtension(BaseExtension):
             client = AsyncIOMotorClient(app_inner.config['MONGODB_URI'])
             setattr(app_inner, self.app_attribute, client)
 
+            database = app_inner.config['MONGODB_DATABASE']
+            if database:
+                motor_database_client = client[database]
+                lazy_instance = app_inner.config['LAZY_UMONGO']
+                lazy_instance.init(motor_database_client)
+
         @app.listener('after_server_stop')
         async def mongodb_free_resources(app_inner, _loop):
             client = getattr(app_inner, self.app_attribute, None)
