c136c08193999802fe3dabdb0ad45b0618dd2aa9
Valeryi Savich <Relrin78@gmail.com>
2018-02-16 20:42:50 +0300

    Update config.py

--- sanic-app/config.py
+++ sanic-app/config.py
@@ -1,5 +1,7 @@
 import os
 
+from umongo import MotorAsyncIOInstance
+
 
 def to_bool(value):
     return str(value).strip().lower() in ['1', 'true', 'yes']
@@ -14,9 +16,11 @@ def to_int(value):
 
 APP_HOST = os.environ.get('APP_HOST', "127.0.0.1")
 APP_PORT = to_int(os.environ.get('APP_HOST', "8000"))
-APP_DEBUG = to_bool(os.environ.get('APP_HOST', False))
+APP_DEBUG = to_bool(os.environ.get('APP_DEBUG', False))
 APP_SSL = None
 APP_WORKERS = int((os.environ.get('APP_WORKERS', 1)))
+
+SECRET_KEY = os.environ.get('APP_SECRET_KEY', 'some-secret-key')
 
 REDIS_HOST = os.environ.get('REDIS_HOST', "127.0.0.1")
 REDIS_PORT = to_int(os.environ.get('REDIS_PORT', "6379"))
@@ -30,7 +34,7 @@ MONGODB_USERNAME = os.environ.get("MONGODB_USERNAME", "user")
 MONGODB_PASSWORD = os.environ.get("MONGODB_PASSWORD", "password")
 MONGODB_HOST = os.environ.get("MONGODB_HOST", "mongodb")
 MONGODB_PORT = to_int(os.environ.get("MONGODB_PORT", 27017))
-MONGODB_DATABASE = os.environ.get("MONGODB_DATABASE", "app")
+MONGODB_DATABASE = os.environ.get("MONGODB_DATABASE", None)
 MONGODB_URI = 'mongodb://{}:{}@{}:{}/{}'.format(
     MONGODB_USERNAME,
     MONGODB_PASSWORD,
@@ -38,3 +42,4 @@ MONGODB_URI = 'mongodb://{}:{}@{}:{}/{}'.format(
     MONGODB_PORT,
     MONGODB_DATABASE
 )
+LAZY_UMONGO = MotorAsyncIOInstance()
